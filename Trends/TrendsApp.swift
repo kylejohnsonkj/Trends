1bf22a991f33d04e4edf97363791146ebb6e61d5
Kyle Johnson <kyjohnson09@gmail.com>
2021-03-27 18:59:51 -0500

    Initial Commit

---
+++ Trends/TrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendsApp.swift
+//  Trends
+//
+//  Created by Kyle Johnson on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct TrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
